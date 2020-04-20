6b72c53b20bc0f4fe85043f8e139734106411bde
FLOWZ <ketuimevbore@yahoo.com>
2020-04-20 13:13:41 -0700

    individual country details now dipslayed in recyclerView

--- app/src/main/java/com/flowz/hackcovid19/MainActivity.java
+++ app/src/main/java/com/flowz/hackcovid19/MainActivity.java
@@ -1,6 +1,8 @@
 package com.flowz.hackcovid19;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -9,6 +11,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.flowz.hackcovid19.adapter.CountryAdapter;
 import com.flowz.hackcovid19.network.ApiClient;
 import com.flowz.hackcovid19.network.ApiInterface;
 import com.flowz.hackcovid19.pojoClasses.Countries;
@@ -19,14 +22,27 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     Countries couDetail;
 
-    TextView text1;
+    RecyclerView rcyCountry;
+
+    TextView newConfirmed, newDeaths, newRecovered, totalConfirmed, totalDeaths, totalRecovered;
+    private Object Context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity1_main);
 
-        text1 = findViewById(R.id.text1);
+        rcyCountry = findViewById(R.id.ryc_country);
+
+       // rcyCountry.setLayoutManager(new LinearLayoutManager(this));
+        rcyCountry.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        newConfirmed = findViewById(R.id.newConfirmedNum);
+        newDeaths = findViewById(R.id.newDeathsNum);
+        newRecovered = findViewById(R.id.newRecoveredNum);
+        totalConfirmed = findViewById(R.id.totalConfirmedNum);
+        totalDeaths = findViewById(R.id.totalDeathsNum);
+        totalRecovered = findViewById(R.id.totalRecoveredNum);
 
         getDataforCountryCode("CountryCode");
     }
@@ -45,43 +61,58 @@ public class MainActivity extends AppCompatActivity {
 
                     Summary fetchedCovidData = response.body();
 //
-//                    int data = fetchedCovidData.getGlobal().getNewConfirmed();
-//
-//                    int data1 = fetchedCovidData.getGlobal().getNewDeaths();
-//
-//                    int data5 = fetchedCovidData.getGlobal().getTotalConfirmed();
-//
-//                    int data2 = fetchedCovidData.getGlobal().getNewRecovered();
-//
-//                    int data3 = fetchedCovidData.getGlobal().getTotalDeaths();
-//
-//                    int data4 = fetchedCovidData.getGlobal().getTotalRecovered();
+                    int data = fetchedCovidData.getGlobal().getNewConfirmed();
 
+                    int data1 = fetchedCovidData.getGlobal().getNewDeaths();
+
+                    int data2 = fetchedCovidData.getGlobal().getNewRecovered();
+
+                    int data5 = fetchedCovidData.getGlobal().getTotalConfirmed();
+
+                    int data3 = fetchedCovidData.getGlobal().getTotalDeaths();
+
+                    int data4 = fetchedCovidData.getGlobal().getTotalRecovered();
+
+                    String nC = String.valueOf(data);
+                    String nD = String.valueOf(data1);
+                    String nR = String.valueOf(data2);
+                    String tC = String.valueOf(data5);
+                    String tD = String.valueOf(data3);
+                    String tR = String.valueOf(data4);
+
+                    newConfirmed.setText(nC);
+                    newDeaths.setText(nD);
+                    newRecovered.setText(nR);
+                    totalConfirmed.setText(tC);
+                    totalDeaths.setText(tD);
+                    totalRecovered.setText(tR);
 
 
                     List<Countries> country = fetchedCovidData.getCountries();
 
+                    loadCountriesRecycler(country);
 
-                    String data1 = country.get(20).getCountry();
-                    String data2 = country.get(20).getCountryCode();
-                    String data3 = country.get(20).getSlug();
-                    String data4 = String.valueOf(country.get(20).getNewConfirmed());
-                    String data5 = String.valueOf(country.get(20).getNewDeaths());
-                    String data6 = String.valueOf(country.get(20).getTotalDeaths());
-                    String data7 = String.valueOf(country.get(20).getNewRecovered());
-                    String data8 = String.valueOf(country.get(20).getTotalRecovered());
-                    String data9 = country.get(20).getDate();
 
-                    text1.append("\n Country :" + data1 +
-                                 "\n CountryCode :" + data2
-                                 +"\n Slug :" + data3
-                                 +"\n NewConfirmed :" + data4
-                                 +"\n NewDeaths :" + data5
-                                 +"\n TotalDeaths :" + data6 +
-                                 "\n NewRecovered :" + data7
-                                 +"\n TotalRecovered :" + data8
-                                 +"\n Date :" + data9
-                    );
+//                    String data1 = country.get(20).getCountry();
+//                    String data2 = country.get(20).getCountryCode();
+//                    String data3 = country.get(20).getSlug();
+//                    String data4 = String.valueOf(country.get(20).getNewConfirmed());
+//                    String data5 = String.valueOf(country.get(20).getNewDeaths());
+//                    String data6 = String.valueOf(country.get(20).getTotalDeaths());
+//                    String data7 = String.valueOf(country.get(20).getNewRecovered());
+//                    String data8 = String.valueOf(country.get(20).getTotalRecovered());
+//                    String data9 = country.get(20).getDate();
+
+//                    text1.append("\n Country :" + data1 +
+//                                 "\n CountryCode :" + data2
+//                                 +"\n Slug :" + data3
+//                                 +"\n NewConfirmed :" + data4
+//                                 +"\n NewDeaths :" + data5
+//                                 +"\n TotalDeaths :" + data6 +
+//                                 "\n NewRecovered :" + data7
+//                                 +"\n TotalRecovered :" + data8
+//                                 +"\n Date :" + data9
+//                    );
 
 
 
@@ -143,6 +174,11 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    private void loadCountriesRecycler(List<Countries> country) {
+        CountryAdapter countryAdapter = new CountryAdapter(country, this);
+        rcyCountry.setAdapter(countryAdapter);
     }
 
 }
